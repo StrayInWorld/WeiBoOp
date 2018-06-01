50945ecdaa99f8682b41584dc9e740db93e6069c
StrayInWorld <strayInWorld@16.com>
2018-06-01 21:58:17 +0800

    完善代码

--- test.py
+++ test.py
@@ -1,31 +1,18 @@
+# coding=utf-8
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
+import time
 
+# 访问百度
 driver = webdriver.Chrome()
-driver.get(r'G:\weiboOp\test.html')
+driver.get("http://www.baidu.com")
 
-# # 1.串联寻找
-# print(driver.find_element_by_id('B').find_element_by_tag_name('div').text)
-#
-# # 2.xpath父子关系寻找
-# print(driver.find_element_by_xpath("//div[@id='B']/div").text)
-#
-# # 3.css selector父子关系寻找
-# print(driver.find_element_by_css_selector('div#B>div').text)
-#
-# # 4.css selector nth-child
-#print(driver.find_element_by_css_selector('div#B div:nth-child(1)').text)
-#
-# # 5.css selector nth-of-type
-# print(driver.find_element_by_css_selector('div#B div:nth-of-type(1)').text)
-#
-# # 6.xpath轴 child
-# print(driver.find_element_by_xpath("//div[@id='B']/child::div").text)
+# 搜索
+driver.find_element_by_id("kw").send_keys("selenium")
+driver.find_element_by_id("su").click()
+time.sleep(3)
 
-# test
-#print(driver.find_element_by_css_selector('div#B:nth-child(2)').text)
-print(driver.find_element_by_css_selector('div#B div').text)
-print(driver.find_element_by_css_selector('div#B>div:nth-child(2)').text)
+driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+print("执行了")
+time.sleep(3)
+
 driver.quit()
-
--- weiBoOIp.py
+++ weiBoOIp.py
@@ -2,12 +2,16 @@
 
 import json
 import os
+import time
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver import ActionChains
+
 
 def OpenBaiDu():
     driver = webdriver.Chrome()
@@ -18,11 +22,13 @@ def OpenBaiDu():
 
 
 class weiBoOpClass(object):
-    def __init__(self,driver,url,findStr):
+    def __init__(self,driver):
          self.driver=driver
-         self.driver.get(url)
-         self.driver.implicitly_wait(6)
-         self.isHaveCookiesFile(findStr)
+
+    def startOp(self,url,keyWord):
+        self.driver.get(url)
+        self.driver.implicitly_wait(3)
+        self.isHaveCookiesFile(keyWord)
 
     def isHaveCookiesFile(self,findStr):
         if os.path.isfile("cookies.json"):
@@ -54,6 +60,23 @@ class weiBoOpClass(object):
             print("找到%s个元素：%s" % (len(s), css))
             return False
 
+    #处理弹框
+    def handlerAlert(self):
+        self.driver.find_element_by_xpath('//*[@id="app"]/div[2]/div[1]/div[2]/footer/div/a').click()
+        print("已确定")
+        self.driver.find_element_by_xpath('//*[@id="app"]/div[1]/div/header/div[1]').click()
+        print("关闭")
+        if self.is_element_exist('//*[@id="app"]/div[1]/div/div[1]/div/div[1]/div'):
+            self.driver.find_element_by_css_selector(
+                '#app > div:nth-child(1) > div > div.m-top-bar.m-panel.m-container-max.m-topbar-max > div > div.nav-left > div').click()
+            print("已返回")
+
+    #移动鼠标
+    def movePage(self):
+        time.sleep(2)
+        self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        print("执行了")
+
     def doOp(self,findKeyWord):
         self.driver.get('https://m.weibo.cn/')
 
@@ -78,6 +101,8 @@ class weiBoOpClass(object):
 
         commendlist = self.driver.find_elements_by_css_selector(".m-ctrl-box.m-box-center-a")
         print(len(commendlist))
+        if len(commendlist)==0:
+            self.movePage()
         for i in range(len(commendlist)):
             # 下面重新获取"转发，评论，赞" 是因为进行下面一系列操作之后，返回到主页面时，内容已经改变，所以需要重新获取
             newcommendlist = self.driver.find_elements_by_css_selector(".m-ctrl-box.m-box-center-a")
@@ -87,30 +112,38 @@ class weiBoOpClass(object):
             # print(btnClick.text)
             print("已点击外部评论")
             #有1条评论以上的才需要二次点击评论
-            if self.is_element_exist('// *[ @ id = "app"] / div[1] / div / div[2] / div / div / footer / div[2]'):
-                self.driver.find_element_by_xpath('// *[ @ id = "app"] / div[1] / div / div[2] / div / div / footer / div[2]').click()  # 内部评论
-                print("已点击内部评论")
+            if not self.is_element_exist('// *[ @ id = "app"] / div[1] / div / div[2] / div / div / footer / div[2]'):
+                self.driver.find_element_by_tag_name("textarea").send_keys(r"谔谔21 " + str(i))  # 评论内容
+                print("已发表评论")
+                self.driver.find_element_by_xpath('//*[@id="app"]/div[1]/div/header/div[3]/a').click()  # 发送评论
+                print("已发送评论")
+                # 弹框处理
+                if self.is_element_exist('//*[@id="app"]/div[2]/div[1]/div[2]/footer/div/a'):
+                    self.handlerAlert()
+                    continue
+                continue
+            self.driver.find_element_by_xpath('// *[ @ id = "app"] / div[1] / div / div[2] / div / div / footer / div[2]').click()  # 内部评论
+            print("已点击内部评论")
             self.driver.find_element_by_tag_name("textarea").send_keys(r"谔谔21 " + str(i))  # 评论内容
             print("已发表评论")
             self.driver.find_element_by_xpath('//*[@id="app"]/div[1]/div/header/div[3]/a').click()  # 发送评论
             print("已发送评论")
             #弹框处理
             if self.is_element_exist('//*[@id="app"]/div[2]/div[1]/div[2]/footer/div/a'):
-                self.driver.find_element_by_xpath('//*[@id="app"]/div[2]/div[1]/div[2]/footer/div/a').click()
-                print("已确定")
-                self.driver.find_element_by_xpath('//*[@id="app"]/div[1]/div/header/div[1]').click()
-                print("关闭")
-                if self.is_element_exist('//*[@id="app"]/div[1]/div/div[1]/div/div[1]/div'):
-                    self.driver.find_element_by_css_selector('#app > div:nth-child(1) > div > div.m-top-bar.m-panel.m-container-max.m-topbar-max > div > div.nav-left > div').click()
-                    print("已返回")
+                self.handlerAlert()
                 continue
-            if self.is_element_exist('//*[@id="app"]/div[1]/div/div[1]/div/div[1]/div'):
-                self.driver.find_element_by_css_selector('#app > div:nth-child(1) > div > div.m-top-bar.m-panel.m-container-max.m-topbar-max > div > div.nav-left > div').click()
-                print("已返回")
+            self.driver.find_element_by_css_selector('#app > div:nth-child(1) > div > div.m-top-bar.m-panel.m-container-max.m-topbar-max > div > div.nav-left > div').click()
+            print("已返回")
         self.driver.quit()
 
-classDriver=weiBoOpClass(webdriver.Chrome(),"https://m.weibo.cn/","范冰冰")
 
+classDriver=weiBoOpClass(webdriver.Chrome())
+try:
+    classDriver.startOp("https://m.weibo.cn/", "范冰冰")
+finally:
+    classDriver.driver.quit()
+    print("出错了，重新运行了")
+    weiBoOpClass(webdriver.Chrome()).startOp("https://m.weibo.cn/", "范冰冰")
 
 
 
